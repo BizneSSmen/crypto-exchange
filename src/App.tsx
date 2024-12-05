4571e88a74b37ca5644b0fcbe8391810b21b6648
Artem <artem@MacBook-Air-Artem.local>
2024-12-05 19:06:51 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Financial } from "./interfaces/financial.interface";
+import axios from "axios";
+import { CurrencyCard } from "./components/currency-card/CurrencyCard";
+import { Exchange } from "./interfaces/exchange.interface";
+import { TargetCardList } from "./components/target-cards/TargetCardList";
+import { SourceAmountInput } from "./components/source-amount-input/SourceAmountInput";
+import { SourceCurrenciesOffcanvas } from "./components/source-currencies-offcanvas/SourceCurrenciesOffcanvas";
+
+const apiUrl: string = process.env.REACT_APP_API_URL!;
 
 function App() {
+  const [sourceCurrencyId, setSourceCurrencyId] = useState<number>(1);
+  const [sourceCurrency, setSourceCurrency] = useState<Financial>({});
+  const [sourceCurrencies, setSourceCurrencies] = useState<Financial[]>([{}]);
+  const [targetCurrencies, setTargetCurrencies] = useState<Exchange[]>([]);
+  const [toggleOffcanvas, setToggleOffcanvas] = useState<() => void>(() => {});
+
+  const fetchSourcecurrency = async (): Promise<void> => {
+    try {
+      const response = await axios.get<Financial>(
+        `${apiUrl}/financial/${sourceCurrencyId}`
+      );
+      setSourceCurrency(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchSourcecurrencies = async (): Promise<void> => {
+    try {
+      const response = await axios.get<Financial[]>(
+        `${apiUrl}/financial/all/${sourceCurrencyId}`
+      );
+      setSourceCurrencies(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchTargetCurrencies = async () => {
+    try {
+      const response = await axios.get<Exchange[]>(
+        `${apiUrl}/exchange/${sourceCurrencyId}`
+      );
+      setTargetCurrencies(response.data);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    fetchSourcecurrency();
+    fetchTargetCurrencies();
+    fetchSourcecurrencies();
+  }, [sourceCurrencyId]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CurrencyCard
+        toggleOffcanvas={toggleOffcanvas}
+        switchCurrencyid={setSourceCurrencyId}
+        {...sourceCurrency}
+      />
+      <SourceAmountInput />
+      <TargetCardList targetCurrencies={targetCurrencies} />
+      <SourceCurrenciesOffcanvas
+        switchCurrencyId={setSourceCurrencyId}
+        setToggleOffcanvas={setToggleOffcanvas}
+        sourceCurrencies={sourceCurrencies}
+      />
     </div>
   );
 }
